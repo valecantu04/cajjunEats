090faf41a813bc463c519e9208597d35a994b5e6
Valentin Cantu <MeroMero@Valentins-MacBook-Pro.local>
2018-12-03 22:19:25 -0600

    created html index, css styling, and js script for smooth scrolling. Landing page still needs to be modified. form needs to be created

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Smooth scroll
+$('#view-work').on('click', function () {
+    const images = $('#images').position().top;
+
+    $('html, body').animate({
+        scrollTop: images
+    }, 900);
+});

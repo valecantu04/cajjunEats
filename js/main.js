8631a6f94bfc891f0d53a96e01345354b7691b23
Valentin Cantu <MeroMero@Valentins-MacBook-Pro.local>
2018-12-05 21:03:46 -0600

    added the form and created form functionality. pending: testing if form works.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+$(document).ready(function (){
+    $('.submit').click(function (event){
+        console.log('click')
+
+        var name = $('.name').val()
+        var number = $('.number').val()
+        var email = $('.email').val()
+        var date = $('.date').val()
+        var address = $('.address').val()
+        var numPeople = $('.num-people').val()
+        var message = $('.message').val()
+        var statusElm = $('.status')
+        statusElm.empty()
+
+        if (name.length == 0 && !email.includes('@') && !email.includes('.')) {
+            event.preventDefault()
+            statusElm.append('<div>Email is invalid</div>')
+        }
+        if(email.length < 4 && !email.includes('@') && !email.includes('.')){
+            event.preventDefault()
+            statusElm.append('<div>Email is invalid</div>')
+        }
+        if (number.length < 9) {
+            event.preventDefault()
+            statusElm.append('<div>Number is invalid</div>')
+        }
+    })
+})
+
 // Smooth scroll
 $('#view-team').on('click', function () {
     const images = $('#team-images').position().top;
@@ -22,3 +51,4 @@ $('#contact-us').on('click', function () {
         scrollTop: contact
     }, 1000);
 });
+

7557c561415a1a97db7889278fa14aa010b25d67
Valentin Cantu <MeroMero@Valentins-MacBook-Pro.local>
2018-12-11 00:41:34 -0600

    implimented slideshow

--- js/main.js
+++ js/main.js
@@ -81,60 +81,65 @@ $('#get-quote').on('click', function () {
     }, 1000);
 });
 
-var slideIndex = 1;
-showSlides(slideIndex);
+$(function () {
+    var inWrap = $('.inner-wrapper');
+    $('.prev').on('click', function () {
+        inWrap.animate({
+            left: '0%'
+        }, 300, function () {
+            inWrap.css('left', '-100%');
+            $('.slide').first().before($('.slide').last());
+        });
+    });
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+    $('.next').on('click', function () {
+        inWrap.animate({
+            left: '-200%'
+        }, 300, function () {
+            inWrap.css('left', '-100%');
+            $('.slide').last().after($('.slide').first());
+        });
+    });
+})
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+$(function () {
+    var inWrap = $('.inner-wrapper1');
+    $('.prev1').on('click', function () {
+        inWrap.animate({
+            left: '0%'
+        }, 300, function () {
+            inWrap.css('left', '-100%');
+            $('.slide1').first().before($('.slide1').last());
+        });
+    });
 
-// Thumbnail image controls
-function currentSlide1(n) {
-    showSlides(slideIndex = n);
-}
+    $('.next1').on('click', function () {
+        inWrap.animate({
+            left: '-200%'
+        }, 300, function () {
+            inWrap.css('left', '-100%');
+            $('.slide1').last().after($('.slide1').first());
+        });
+    });
+})
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+$(function () {
+    var inWrap = $('.inner-wrapper2');
+    $('.prev2').on('click', function () {
+        inWrap.animate({
+            left: '0%'
+        }, 300, function () {
+            inWrap.css('left', '-100%');
+            $('.slide2').first().before($('.slide2').last());
+        });
+    });
 
-function showSlides1(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides1");
-    var dots = document.getElementsByClassName("dot1");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+    $('.next2').on('click', function () {
+        inWrap.animate({
+            left: '-200%'
+        }, 300, function () {
+            inWrap.css('left', '-100%');
+            $('.slide2').last().after($('.slide2').first());
+        });
+    });
+})

70da882203f76082d6d561f498b98a1f6c9b4bda
Valentin Cantu <MeroMero@Valentins-MacBook-Pro.local>
2019-06-28 11:22:16 -0500

    added instagram to main page

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '7530937511',
+        limit: 10,
+        resolution: 'standard_resolution',
+        accessToken: '7530937511.1677ed0.35389b308fff443cb2f876b80a70bd6b',
+        sortBy: 'most-recent',
+        template: '<div class="col-lg-4 gallery instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+    });
+
+
+    userFeed.run();
+
+
+    // This will create a single gallery from all elements that have class "gallery-item"
+    $('.gallery').magnificPopup({
+        type: 'image',
+        delegate: 'a',
+        gallery: {
+            enabled: true
+        }
+    });
+
+
+});

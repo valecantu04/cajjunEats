a839433a2b1679b2a3875643dc2d9bc533c1f61c
Valentin Cantu <MeroMero@Valentins-MacBook-Pro.local>
2019-06-28 11:37:44 -0500

    deleted magnific

--- js/custom.js
+++ js/custom.js
@@ -13,16 +13,4 @@ $(document).ready(function () {
 
 
     userFeed.run();
-
-
-    // This will create a single gallery from all elements that have class "gallery-item"
-    $('.gallery').magnificPopup({
-        type: 'image',
-        delegate: 'a',
-        gallery: {
-            enabled: true
-        }
-    });
-
-
 });
